a4ed6eee4131c085c1095dd8cb0f2c53c0ae8a02
SJB7777 <esakyong1866@naver.com>
2025-03-23 20:15:53 +0900

    Add more descriptions in README.md

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # PAL-XFEL Data Handling Toolkit for Time-Resolved Ultrafast Experiments
 
+---
+# Usage
 ## Prerequisites
 
 - [Anaconda](https://www.anaconda.com/products/distribution) or [Miniconda](https://docs.conda.io/en/latest/miniconda.html) installed
+- [Git](https://git-scm.com/) installed
 
 ## Setting Up the Environment
 
@@ -12,9 +15,10 @@
     cd STO-XFEL
     ```
 
-2. **Updating the Environment:**
-    If you need to update the environment with new packages, you can do so by modifying the `environment.yaml` file and then running:
+2. **Create the virtual environment:**
     ```bash
+    conda create -n xfel python=3.13
+    conda activate xfel
     pip install -r requirements.txt
     ```
 
@@ -55,12 +59,11 @@ param:
    - `analysis_dir`: Root directory for analysis outputs
    - Adjust other paths as needed while maintaining the directory structure
 
-## Usage
-
-Run the processing script with:
+## Integrating
+Run the integrating script with:
 
 ```bash
-python processing_main.py
+python integrating_main.py
 ```
 
 The script will:
@@ -69,6 +72,11 @@ The script will:
 3. Use detector parameters from the configuration
 4. Save outputs in the specified directory structure
 5. Store logs in `./logs`
+
+## Analyzing
+```bash
+python analyzing_main.py
+```
 
 ## Important Parameters
 - `runs`: List of run numbers to process
@@ -79,20 +87,31 @@ The script will:
 
 Note: Ensure all path locations are accessible and have proper read/write permissions before running the script.
 
-## Project Overview
+---
+# Project Overview
 The **PAL-XFEL Data Handling Toolkit** is a specialized suite of tools developed for the efficient processing and analysis of PAL-XFEL data, with a focus on time-resolved ultrafast experiments studying polaronic lattice distortions in Strontium Titanate (STO) nanocrystals. This toolkit is designed to enhance data handling, reduce processing times, and ensure the integrity of analytical results, making it an essential asset for our research on perovskite-oxide materials.
+
+## File Structure of Raw Data
+```bash
+p0110.h5
+├── detector
+│   └── eh1
+│      └── jungfrau2
+│         └── image
+├── metadata
+├── oxc
+├── pd
+├── qbpm
+│   └── eh1
+│      └── qbpm1
+└── oh
+```
 
 ## Goals
 - **Efficient Data Management**: Utilize HDF5 for structured storage and rapid data access, tailored for time-resolved datasets.
 - **Accelerated Data Processing**: Implement parallel processing techniques to speed up I/O operations, critical for handling the rapid sequence of ultrafast experiments.
 - **Data Quality Assurance**: Apply preprocessing methods to clean and standardize data for reliable analysis, ensuring precision in time-resolved measurements.
 - **Automated Workflows**: Develop Python scripts to automate the data processing pipeline, from raw data to analytical outputs, streamlining the analysis of complex ultrafast phenomena.
-
-## Key Components
-- **HDF5 Integration**: For organizing and managing large, time-resolved datasets.
-- **Parallel Processing**: To handle data-intensive tasks more efficiently, crucial for processing the high-frequency data generated in ultrafast experiments.
-- **Preprocessing Algorithms**: To remove anomalies and normalize data, essential for maintaining the accuracy of time-resolved analyses.
-- **Python Scripts**: For automating data workflows and analysis, facilitating the rapid interpretation of ultrafast experimental results.
 
 ## Future Enhancements
 - **Scalability**: Continue to optimize the toolkit to handle increasing data volumes, ensuring it remains effective as experimental datasets grow.
---
+++ analyzing_main.py
@@ -0,0 +1,134 @@
+import os
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
+
+import numpy as np
+import numpy.typing as npt
+import matplotlib.pyplot as plt
+import tifffile
+from roi_rectangle import RoiRectangle
+
+from src.gui.roi import RoiSelector
+from src.filesystem import make_run_scan_directory
+from src.config.config import load_config
+from src.logger import setup_logger, Logger
+from src.analyzer.draw_figure import (
+    patch_rectangle,
+    draw_com_figure,
+    draw_intensity_figure,
+    draw_intensity_diff_figure,
+    draw_com_diff_figure
+)
+from src.analyzer.core import DataAnalyzer
+
+if TYPE_CHECKING:
+    from pandas import DataFrame
+    from matplotlib.figure import Figure
+    from src.config.config import ExpConfig
+
+
+def main() -> None:
+    config: ExpConfig = load_config()
+    logger: Logger = setup_logger()
+
+    run_nums: list[int] = [161, 162, 163, 164, 165, 166, 167]
+    logger.info(f"Data Analysing run={run_nums}")
+    for run_num in run_nums:  # pylint: disable=not-an-iterable
+        # Define run and scan numbers
+        scan_num: int = 1
+        now = datetime.now()
+        roi_name: str = now.strftime("%Y%m%d_%H%M%S")
+
+        # Define file paths and names
+        processed_dir: str = config.path.processed_dir
+        file_name: str = f"run={run_num:0>4}_scan={scan_num:0>4}"
+        npz_dir: str = make_run_scan_directory(processed_dir, run_num, scan_num)
+        npz_file: str = os.path.join(npz_dir, file_name + ".npz")
+
+        if not os.path.exists(npz_file):
+            error_message = f"The file {npz_file} does not exist."
+            logger.error(error_message)
+            raise FileNotFoundError(error_message)
+
+        logger.info(f"Run DataAnalyzer run={run_num:0>3} scan={scan_num:0>3}")
+
+        # Initialize MeanDataProcessor
+        processor: DataAnalyzer = DataAnalyzer(npz_file)
+
+        # Extract images
+        poff_images: npt.NDArray = processor.poff_images
+        pon_images: npt.NDArray = processor.pon_images
+
+        # Select ROI using GUI
+        roi: Optional[tuple[int, int, int, int]] = RoiSelector().select_roi(
+            np.log1p(poff_images[0])
+        )
+        if roi is None:
+            logger.error(f"No ROI Rectangle Set for run={run_num}, scan={scan_num}")
+            raise ValueError(f"No ROI Rectangle Set for run={run_num}, scan={scan_num}")
+
+        logger.info(f"ROI rectangle: {roi}")
+        roi_rect: RoiRectangle = RoiRectangle.from_tuple(roi)
+
+        # Analyze data within the selected ROI
+        data_df: DataFrame = processor.analyze_by_roi(roi_rect)
+
+        # Define save directory
+        output_root: str = config.path.output_dir
+        output_dir: str = os.path.join(make_run_scan_directory(output_root, run_num, scan_num), roi_name)
+        os.makedirs(output_dir, exist_ok=True)
+
+        # Slice images to ROI
+        roi_poff_images: npt.NDArray = roi_rect.slice(poff_images)
+        roi_pon_images: npt.NDArray = roi_rect.slice(pon_images)
+
+        # Save images as TIFF files
+        tifffile.imwrite(os.path.join(output_dir, "poff.tif"), poff_images.astype(np.float32))
+        logger.info(f"Saved TIF '{os.path.join(output_dir, 'poff.tif')}'")
+
+        tifffile.imwrite(os.path.join(output_dir, 'pon.tif'), pon_images.astype(np.float32))
+        logger.info(f"Saved TIF '{os.path.join(output_dir, 'pon.tif')}'")
+
+        tifffile.imwrite(os.path.join(output_dir, "roi_poff.tif"), roi_poff_images.astype(np.float32))
+        logger.info(f"Saved TIF '{os.path.join(output_dir, 'roi_poff.tif')}'")
+
+        tifffile.imwrite(os.path.join(output_dir, "roi_pon.tif"), roi_pon_images.astype(np.float32))
+        logger.info(f"Saved TIF '{os.path.join(output_dir, 'roi_pon.tif')}'")
+
+        # Save data as CSV
+        data_file: str = os.path.join(output_dir, "data.csv")
+        data_df.to_csv(data_file)
+        logger.info(f"Saved CSV '{data_file}'")
+
+        # Create figures
+        image_fig: Figure = patch_rectangle(
+            np.log1p(processor.poff_images.sum(axis=0)),
+            *roi_rect.to_tuple()
+        )
+        intensity_fig: Figure = draw_intensity_figure(data_df)
+        intensity_diff_fig: Figure = draw_intensity_diff_figure(data_df)
+        com_fig: Figure = draw_com_figure(data_df)
+        com_diff_fig: Figure = draw_com_diff_figure(data_df)
+
+        # Save figures as PNG files
+        image_fig.savefig(os.path.join(output_dir, "log_image.png"))
+        logger.info(f"Saved PNG '{os.path.join(output_dir, 'log_image.png')}'")
+
+        intensity_fig.savefig(os.path.join(output_dir, "delay-intensity.png"))
+        logger.info(f"Saved PNG '{os.path.join(output_dir, 'delay-intensity.png')}'")
+
+        intensity_diff_fig.savefig(os.path.join(output_dir, "delay-intensity_diff.png"))
+        logger.info(f"Saved PNG '{os.path.join(output_dir, 'delay-intensity_diff.png')}'")
+
+        com_fig.savefig(os.path.join(output_dir, "delay-com.png"))
+        logger.info(f"Saved PNG '{os.path.join(output_dir, 'delay-com.png')}'")
+
+        com_diff_fig.savefig(os.path.join(output_dir, "delay-com_diff.png"))
+        logger.info(f"Saved PNG '{os.path.join(output_dir, 'delay-com_diff.png')}'")
+
+        logger.info(f"Run DataAnalyzer run={run_num:0>3} scan={scan_num:0>3} is Done.")
+        plt.close("all")
+
+
+if __name__ == "__main__":
+    main()
